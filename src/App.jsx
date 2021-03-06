d292095d0222f93d2ab50b7be6571aada7044829
Angela Huang <angelahuang89@gmail.com>
2021-03-06 01:47:31 -0800

    Rename App and index components and update styling

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Directory } from './components/Directory';
+import './App.css';
+
+export const App = () => {
+  return (
+    <div className="App">
+      <Directory />
+    </div>
+  );
+};
